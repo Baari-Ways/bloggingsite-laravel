4ec119652c08a91b6fce306db44092041bde4627
FareedKhan12 <51131990+FareedKhan12@users.noreply.github.com>
2020-03-30 18:36:58 +0500

    Blogging-site
    Simple Blogging site

---
+++ lsapp/resources/views/pages/ownwork.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="jumbotron">
+    <h4></h4>
+    <h4>This page is for your own use.</h4>
+    <hr>
+    <p>the location of this page is: /resources/views/pages/ownwork.blade.php</p>
+
+
+
+  </div>
+@endsection
+
